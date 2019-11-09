d7d210a0c4f7a789a4025c962e538dc67e19e36c
Luis Cabrera <luisca@microsoft.com>
2019-11-09 05:18:14 -0800

    I noticed it is possible for a page to return null for the cluster id.

--- Vision/AnalyzeForm/Page.cs
+++ Vision/AnalyzeForm/Page.cs
@@ -8,7 +8,7 @@ namespace AzureCognitiveSearch.PowerSkills.Vision.AnalyzeForm
         public int Number { get; set; }
         public int Height { get; set; }
         public int Width { get; set; }
-        public int ClusterId { get; set; }
+        public int? ClusterId { get; set; }
 
         public KeyValuePair[] KeyValuePairs { get; set; }
     }
