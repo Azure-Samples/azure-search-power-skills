502c818c2381961ea96e8f98ff2128a9fcd77589
Bertrand Le Roy <bertrandleroy@gmail.com>
2020-02-27 15:15:37 -0800

    Update auth method for Bing Entity search and Azure Maps from headers to query string parameters

--- Common/WebAPISkillHelper.cs
+++ Common/WebAPISkillHelper.cs
@@ -70,10 +70,19 @@ namespace AzureCognitiveSearch.PowerSkills.Common
             return response;
         }
 
+        public static async Task<IEnumerable<T>> FetchAsync<T>(string uri, string collectionPath)
+            => await FetchAsync<T>(uri, null, null, collectionPath, HttpMethod.Get);
+
         public static async Task<IEnumerable<T>> FetchAsync<T>(string uri, string apiKeyHeader, string apiKey, string collectionPath)
-        {
-            return await FetchAsync<T>(uri, apiKeyHeader, apiKey, collectionPath, HttpMethod.Get);
-        }
+            => await FetchAsync<T>(uri, apiKeyHeader, apiKey, collectionPath, HttpMethod.Get);
+
+        public static async Task<IEnumerable<T>> FetchAsync<T>(
+            string uri,
+            string collectionPath,
+            HttpMethod method,
+            byte[] postBody = null,
+            string contentType = null)
+            => await FetchAsync<T>(uri, null, null, collectionPath, method, postBody, contentType);
 
         public static async Task<IEnumerable<T>> FetchAsync<T>(
             string uri,
@@ -97,7 +106,10 @@ namespace AzureCognitiveSearch.PowerSkills.Common
                 {
                     request.Content.Headers.ContentType = new MediaTypeHeaderValue(contentType);
                 }
-                request.Headers.Add(apiKeyHeader, apiKey);
+                if (apiKeyHeader != null)
+                {
+                    request.Headers.Add(apiKeyHeader, apiKey);
+                }
 
                 using (HttpResponseMessage response = TestMode ? TestWww(request) : await client.SendAsync(request))
                 {
@@ -120,6 +132,5 @@ namespace AzureCognitiveSearch.PowerSkills.Common
                 }
             }
         }
-
     }
 }
