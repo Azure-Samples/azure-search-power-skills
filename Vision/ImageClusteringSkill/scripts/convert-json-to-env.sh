c1068a55dcb6192fe96b41c0410ba5268020440f
Stuart Leeks <stuartle@microsoft.com>
2021-06-29 12:44:04 +0100

    Quote values in env files

--- Vision/ImageClusteringSkill/scripts/convert-json-to-env.sh
+++ Vision/ImageClusteringSkill/scripts/convert-json-to-env.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -e
 
-#
-# This script converts Terraform JSON output to .env files
-#
+# Convert the terraform outputs (for string values) to env files
+# prefixing the key names with TF_VAR_
+# values are formatted as KEY=$'VALUE' with any single quotes escaped as \'
+# e.g. a value of (MY'VALUE) would result in: KEY=$'MY\'VALUE'
 
-jq -r 'to_entries | .[] | select(.value.type == "string") | "TF_VAR_\(.key)=\(.value.value)"' 
+jq -r "to_entries | .[] | select(.value.type == \"string\") | \"TF_VAR_\\(.key)=\$'\\(.value.value | gsub(\"(?<x>('))\"; \"\\\\'\"))'\"" 
