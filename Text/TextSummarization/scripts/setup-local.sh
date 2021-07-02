516cc4b60b0816a0c63e3271cfa655a20d191fe9
ross-p-smith <rosmith@microsoft.com>
2021-07-02 22:34:57 +0100

    PR comments

--- Text/TextSummarization/scripts/setup-local.sh
+++ Text/TextSummarization/scripts/setup-local.sh
@@ -6,7 +6,7 @@ script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd "$script_dir/../powerskill"
 
 if [ ! -f ".env" ]; then
-    echo "ERROR: Rename '/powerskill/env.example' to '/powerskill/.env' and add you values in."
+    echo "ERROR: Rename '/powerskill/.env.example' to '/powerskill/.env' and add you values in."
     exit 1
 fi
 
