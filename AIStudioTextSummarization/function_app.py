1149d091d4a4032bb416e86c17650572fe60a1c4
Amit Kalay <47650976+amitkalay@users.noreply.github.com>
2024-10-03 17:15:02 +0000

    created the POST function to the future summarizer endpoint!

--- AIStudioTextSummarization/function_app.py
+++ AIStudioTextSummarization/function_app.py
@@ -4,14 +4,6 @@ import logging
 
 app = func.FunctionApp()
 
-@app.route(route="HttpExample", auth_level=func.AuthLevel.ANONYMOUS)
-def HttpExample(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
-    name = req.params.get('name')
-    if not name:
-        name = "SOME_DEFAULT_NAME"
-    return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-
 # the healthcheck endpoint. Important to make sure that deployments are healthy
 @app.route(route="health", auth_level=func.AuthLevel.ANONYMOUS)
 def HealthCheck(req: func.HttpRequest) -> func.HttpResponse:
@@ -19,4 +11,61 @@ def HealthCheck(req: func.HttpRequest) -> func.HttpResponse:
     response_body = { "status": "Healthy" }
     response = func.HttpResponse(json.dumps(response_body, default=lambda obj: obj.__dict__))
     response.headers['Content-Type'] = 'application/json'    
+    return response
+
+'''
+the sample payload for the summarization skill will look like this:
+{
+    "values": [
+      {
+        "recordId": "0",
+        "data":
+           {
+             "text": "Este es un contrato en Inglés",
+             "language": "es",
+             "phraseList": ["Este", "Inglés"]
+           }
+      },
+      {
+        "recordId": "1",
+        "data":
+           {
+             "text": "Hello world",
+             "language": "en",
+             "phraseList": ["Hi"]
+           }
+      },
+      {
+        "recordId": "2",
+        "data":
+           {
+             "text": "Hello world, Hi world",
+             "language": "en",
+             "phraseList": ["world"]
+           }
+      },
+      {
+        "recordId": "3",
+        "data":
+           {
+             "text": "Test",
+             "language": "es",
+             "phraseList": []
+           }
+      }
+    ]
+}
+'''
+
+@app.function_name(name="TextSummarizer")
+@app.route(route="summarize")
+def text_chunking(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Received a request to the summarize endpoint.')
+    request_json = req.get_json()
+    logging.info(f'the request_json is: {request_json}')
+    values = []
+    response_body = { "values": values }
+    response = func.HttpResponse(json.dumps(response_body, default=lambda obj: obj.__dict__))
+    response.headers['Content-Type'] = 'application/json'
+    logging.info("Sucessfully returned the response body!")
     return response
