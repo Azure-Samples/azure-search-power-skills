63e4136e36da249fcfd8e46c553287f3c24c0b58
Amit Kalay <47650976+amitkalay@users.noreply.github.com>
2024-10-03 18:26:21 +0000

    unable to import the jsonschema library for some reason...

--- AIStudioTextSummarization/function_app.py
+++ AIStudioTextSummarization/function_app.py
@@ -1,6 +1,7 @@
 import azure.functions as func
 import json
 import logging
+# import jsonschema
 
 app = func.FunctionApp()
 
@@ -63,9 +64,44 @@ def text_chunking(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Received a request to the summarize endpoint.')
     request_json = req.get_json()
     logging.info(f'the request_json is: {request_json}')
+    #try:
+    #    jsonschema.validate(request_json, schema=get_summarization_request_schema())
+    #except jsonschema.exceptions.ValidationError as e:
+    #    return func.HttpResponse("Invalid request: {0}".format(e), status_code=400)
     values = []
     response_body = { "values": values }
     response = func.HttpResponse(json.dumps(response_body, default=lambda obj: obj.__dict__))
     response.headers['Content-Type'] = 'application/json'
     logging.info("Sucessfully returned the response body!")
     return response
+
+# this function describes the expected schema of the json payload
+# for more information, go here: https://learn.microsoft.com/en-us/azure/search/cognitive-search-custom-skill-web-api
+'''
+def get_summarization_request_schema() -> dict:
+    return {
+        "$schema": "http://json-schema.org/draft-04/schema#",
+        "type": "object",
+        "properties": {
+            "values": {
+                "type": "array",
+                "minItems": 1,
+                "items": {
+                    "type": "object",
+                    "properties": {
+                        "recordId": {"type": "string"},
+                        "data": {
+                            "type": "object",
+                            "properties": {
+                                "text": {"type": "string", "minLength": 1}
+                            },
+                            "required": ["text"],
+                        },
+                    },
+                    "required": ["recordId", "data"],
+                },
+            }
+        },
+        "required": ["values"],
+    }
+'''
