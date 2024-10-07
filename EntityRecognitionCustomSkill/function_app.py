97d51ce4843906c010544660fdc2bdf5af6d54e5
Amit Kalay <47650976+amitkalay@users.noreply.github.com>
2024-10-07 22:49:55 +0000

    added json schema validation in the Azure function

--- EntityRecognitionCustomSkill/function_app.py
+++ EntityRecognitionCustomSkill/function_app.py
@@ -1,6 +1,8 @@
 import azure.functions as func
 import logging
 import json
+import jsonschema
+
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
@@ -17,4 +19,44 @@ def HealthCheck(req: func.HttpRequest) -> func.HttpResponse:
     response.headers["Content-Type"] = "application/json"
     return response
 
+@app.function_name(name="EntityRecognition")
+@app.route(route="entity_recognition")
+def entity_recognition(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Entity Recognition endpoint received a request.')
+    request = req.get_json()
+    try:
+        jsonschema.validate(request, schema=get_request_schema())
+    except jsonschema.exceptions.ValidationError as e:
+        return func.HttpResponse("Invalid request: {0}".format(e), status_code=400)
+    values = []
+    response_body = { "values": values }
+    response = func.HttpResponse(json.dumps(response_body, default=lambda obj: obj.__dict__))
+    response.headers['Content-Type'] = 'application/json' 
+    return response
 
+def get_request_schema():
+    return {
+        "$schema": "http://json-schema.org/draft-04/schema#",
+        "type": "object",
+        "properties": {
+            "values": {
+                "type": "array",
+                "minItems": 1,
+                "items": {
+                    "type": "object",
+                    "properties": {
+                        "recordId": {"type": "string"},
+                        "data": {
+                            "type": "object",
+                            "properties": {
+                                "text": {"type": "string", "minLength": 1}
+                            },
+                            "required": ["text"],
+                        },
+                    },
+                    "required": ["recordId", "data"],
+                },
+            }
+        },
+        "required": ["values"],
+    }
