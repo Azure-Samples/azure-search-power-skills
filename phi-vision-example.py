186716be646eb0bb35e7771dd8a6d4df228467dc
Amit Kalay <47650976+amitkalay@users.noreply.github.com>
2024-10-30 20:32:07 +0000

    working via vanilla http call

--- phi-vision-example.py
+++ phi-vision-example.py
@@ -1,52 +1,66 @@
-# pip install azure-ai-inference
+# ------------------------------------
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+# ------------------------------------
+"""
+DESCRIPTION:
+    This sample demonstrates how to get a chat completions response from
+    the service using a synchronous client.
+
+    This sample assumes the AI model is hosted on a Serverless API or
+    Managed Compute endpoint. For GitHub Models or Azure OpenAI endpoints,
+    the client constructor needs to be modified. See package documentation:
+    https://github.com/Azure/azure-sdk-for-python/blob/main/sdk/ai/azure-ai-inference/README.md#key-concepts
+
+USAGE:
+    python sample_chat_completions.py
+
+    Set these two environment variables before running the sample:
+    1) AZURE_AI_CHAT_ENDPOINT - Your endpoint URL, in the form 
+        https://<your-deployment-name>.<your-azure-region>.models.ai.azure.com
+        where `your-deployment-name` is your unique AI Model deployment name, and
+        `your-azure-region` is the Azure region where your model is deployed.
+    2) AZURE_AI_CHAT_KEY - Your model key (a 32-character string). Keep it secret.
+"""
 import os
+import requests
 from azure.ai.inference import ChatCompletionsClient
+from azure.ai.inference.models import SystemMessage, UserMessage
 from azure.core.credentials import AzureKeyCredential
-from dotenv import load_dotenv
 
-load_dotenv()
+def sample_chat_completions():
+    try:
+        endpoint = os.environ["CHAT_COMPLETION_ENDPOINT"]
+        key = os.environ["AZURE_INFERENCE_CREDENTIAL"]
+    except KeyError:
+        print("Missing environment variable 'AZURE_AI_CHAT_ENDPOINT' or 'AZURE_AI_CHAT_KEY'")
+        print("Set them before running this sample.")
+        exit()
 
-api_key = os.getenv("AZURE_INFERENCE_CREDENTIAL", '')
-endpoint = os.getenv("CHAT_COMPLETION_ENDPOINT", '')
-if not api_key:
-  raise Exception("A key should be provided to invoke the endpoint")
-
-client = ChatCompletionsClient(
-    endpoint=endpoint,
-    credential=AzureKeyCredential(api_key)
-)
-
-model_info = client.get_model_info()
-print("Model name:", model_info.model_name)
-print("Model type:", model_info.model_type)
-print("Model provider name:", model_info.model_provider_name)
-
-payload = {
-  "messages": [
-    {
-      "role": "user",
-      "content": "I am going to Paris, what should I see?"
-    },
-    {
-      "role": "assistant",
-      "content": "Paris, the capital of France, is known for its stunning architecture, art museums, historical landmarks, and romantic atmosphere. Here are some of the top attractions to see in Paris:\n\n1. The Eiffel Tower: The iconic Eiffel Tower is one of the most recognizable landmarks in the world and offers breathtaking views of the city.\n2. The Louvre Museum: The Louvre is one of the world's largest and most famous museums, housing an impressive collection of art and artifacts, including the Mona Lisa.\n3. Notre-Dame Cathedral: This beautiful cathedral is one of the most famous landmarks in Paris and is known for its Gothic architecture and stunning stained glass windows.\n\nThese are just a few of the many attractions that Paris has to offer. With so much to see and do, it's no wonder that Paris is one of the most popular tourist destinations in the world."
-    },
-    {
-      "role": "user",
-      "content": "What is so great about #1?"
+    client = ChatCompletionsClient(endpoint=endpoint, credential=AzureKeyCredential(key))
+    hardcoded_payload = {"messages":[{"role":"system","content":"You are an AI assistant that helps people find information."},{"role":"user","content":"Mile to feet"}],"max_tokens":2048,"temperature":0.8,"top_p":0.1,"presence_penalty":0,"frequency_penalty":0,"stream": False}
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": key,
     }
-  ],
-  "max_tokens": 2048,
-  "temperature": 0.8,
-  "top_p": 0.1,
-  "presence_penalty": 0,
-  "frequency_penalty": 0
-}
-response = client.complete(payload)
+    # THIS ONE WORKS!!
+    response_via_http = requests.post(endpoint, headers=headers, json=hardcoded_payload)
+    
+    try:
+      # THIS ONE FAILS
+      response = client.complete(
+          messages=[
+              SystemMessage(content="You are a helpful assistant."),
+              UserMessage(content="How many feet are in a mile?"),
+          ]
+      )
 
-print("Response:", response.choices[0].message.content)
-print("Model:", response.model)
-print("Usage:")
-print("	Prompt tokens:", response.usage.prompt_tokens)
-print("	Total tokens:", response.usage.total_tokens)
-print("	Completion tokens:", response.usage.completion_tokens)
+    except Exception as e:
+      print(e)
+
+    print(response.choices[0].message.content)
+    # [END chat_completions]
+
+
+if __name__ == "__main__":
+    sample_chat_completions()
