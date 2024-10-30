ee409c60a02ff76ac57ed7f7ba6536219f2e5b01
Amit Kalay <47650976+amitkalay@users.noreply.github.com>
2024-10-30 06:05:50 +0000

    better formatting

--- AzureAIStudioCustomInferenceSkill/function_app.py
+++ AzureAIStudioCustomInferenceSkill/function_app.py
@@ -139,27 +139,27 @@ def call_chat_completion_model(request_body: dict, scenario: str):
     "frequency_penalty": 0
     }
 
-    logging.info(f'the request payload is: {payload}')
+    # logging.info(f'the request payload is: {payload}')
     hardcoded_payload = {
-    "messages": [
-    {
-      "role": "user",
-      "content": "I am going to Paris, what should I see?"
-    },
-    {
-      "role": "assistant",
-      "content": "Paris, the capital of France, is known for its stunning architecture, art museums, historical landmarks, and romantic atmosphere. Here are some of the top attractions to see in Paris:\n\n1. The Eiffel Tower: The iconic Eiffel Tower is one of the most recognizable landmarks in the world and offers breathtaking views of the city.\n2. The Louvre Museum: The Louvre is one of the world's largest and most famous museums, housing an impressive collection of art and artifacts, including the Mona Lisa.\n3. Notre-Dame Cathedral: This beautiful cathedral is one of the most famous landmarks in Paris and is known for its Gothic architecture and stunning stained glass windows.\n\nThese are just a few of the many attractions that Paris has to offer. With so much to see and do, it's no wonder that Paris is one of the most popular tourist destinations in the world."
-    },
-    {
-      "role": "user",
-      "content": "What is so great about #1?"
-    }
-  ],
-  "max_tokens": 2048,
-  "temperature": 0.8,
-  "top_p": 0.1,
-  "presence_penalty": 0,
-  "frequency_penalty": 0
+        "messages": [
+        {
+        "role": "user",
+        "content": "I am going to Paris, what should I see?"
+        },
+        {
+        "role": "assistant",
+        "content": "Paris, the capital of France, is known for its stunning architecture, art museums, historical landmarks, and romantic atmosphere. Here are some of the top attractions to see in Paris:\n\n1. The Eiffel Tower: The iconic Eiffel Tower is one of the most recognizable landmarks in the world and offers breathtaking views of the city.\n2. The Louvre Museum: The Louvre is one of the world's largest and most famous museums, housing an impressive collection of art and artifacts, including the Mona Lisa.\n3. Notre-Dame Cathedral: This beautiful cathedral is one of the most famous landmarks in Paris and is known for its Gothic architecture and stunning stained glass windows.\n\nThese are just a few of the many attractions that Paris has to offer. With so much to see and do, it's no wonder that Paris is one of the most popular tourist destinations in the world."
+        },
+        {
+        "role": "user",
+        "content": "What is so great about #1?"
+        }
+        ],
+        "max_tokens": 2048,
+        "temperature": 0.8,
+        "top_p": 0.1,
+        "presence_penalty": 0,
+        "frequency_penalty": 0
     }
     response = client.complete(hardcoded_payload)
     top_response_text = response.choices[0].message.content
