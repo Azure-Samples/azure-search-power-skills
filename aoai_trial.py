4dc12a6277bb82f6e955668963c6630a633336b5
Amit Kalay <47650976+amitkalay@users.noreply.github.com>
2025-01-15 21:30:34 +0000

    added code for the aoai model as well

---
+++ aoai_trial.py
@@ -0,0 +1,58 @@
+
+import os  
+import base64
+from openai import AzureOpenAI  
+
+endpoint = os.getenv("ENDPOINT_URL", "https://azs-grok-aoai.openai.azure.com/")  
+deployment = os.getenv("DEPLOYMENT_NAME", "azs-grok-gpt-4o")  
+subscription_key = os.getenv("AZURE_OPENAI_API_KEY", "REPLACE_WITH_YOUR_KEY_VALUE_HERE")  
+
+# Initialize Azure OpenAI Service client with key-based authentication    
+client = AzureOpenAI(  
+    azure_endpoint=endpoint,  
+    api_key=subscription_key,  
+    api_version="2024-05-01-preview",
+)  
+   
+# IMAGE_PATH = "YOUR_IMAGE_PATH"
+# encoded_image = base64.b64encode(open(IMAGE_PATH, 'rb').read()).decode('ascii')
+
+# Prepare the chat prompt 
+chat_prompt = [
+    {
+        "role": "system",
+        "content": [
+            {
+                "type": "text",
+                "text": "You are an AI assistant that helps people find information."
+            }
+        ]
+    },
+    {
+        "role": "user",
+        "content": [
+            {
+                "type": "text",
+                "text": "How can I get better at pickleball?"
+            }
+        ]
+    }
+] 
+    
+# Include speech result if speech is enabled  
+messages = chat_prompt  
+    
+# Generate the completion  
+completion = client.chat.completions.create(  
+    model=deployment,
+    messages=messages,
+    max_tokens=800,  
+    temperature=0.7,  
+    top_p=0.95,  
+    frequency_penalty=0,  
+    presence_penalty=0,
+    stop=None,  
+    stream=False
+)
+
+print(completion.to_json())
